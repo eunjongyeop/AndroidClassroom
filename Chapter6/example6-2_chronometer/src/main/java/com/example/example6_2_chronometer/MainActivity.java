6f254dc0a0103c94ea0b0e91f50617fa6a1a7727
eunjongyeop <48788795+eunjongyeop@users.noreply.github.com>
2020-04-17 11:05:21 +0900

    chapter6 과제 UI

---
+++ Chapter6/example6-2_chronometer/src/main/java/com/example/example6_2_chronometer/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.example6_2_chronometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Chronometer chronometer;
+    Button btnStart, btnStop, btnReset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chronometer);
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+        btnReset = findViewById(R.id.btnReset);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.start();
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.stop();
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.setBase(SystemClock.elapsedRealtime());
+            }
+        });
+    }
+}
