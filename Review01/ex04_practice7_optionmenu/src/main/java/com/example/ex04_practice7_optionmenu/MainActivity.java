62a8b5e20f19da460bbf7cf623267d240db4eb44
eunjongyeop <48788795+eunjongyeop@users.noreply.github.com>
2020-06-10 11:41:45 +0900

    .

---
+++ Review01/ex04_practice7_optionmenu/src/main/java/com/example/ex04_practice7_optionmenu/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ex04_practice7_optionmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu1, menu);
+    }
+}
