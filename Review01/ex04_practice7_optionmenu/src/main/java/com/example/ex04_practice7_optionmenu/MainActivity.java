3b86fe3d369e5af74f53441109d406023bba2cbd
eunjongyeop <48788795+eunjongyeop@users.noreply.github.com>
2020-06-10 13:19:58 +0900

    복습 4번 과제

--- Review01/ex04_practice7_optionmenu/src/main/java/com/example/ex04_practice7_optionmenu/MainActivity.java
+++ Review01/ex04_practice7_optionmenu/src/main/java/com/example/ex04_practice7_optionmenu/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.ex04_practice7_optionmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,5 +26,16 @@ public class MainActivity extends AppCompatActivity {
 
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu1, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu3)
+            return false;
+        textView.setText(item.getTitle()+" 선택됨");
+
+        return true;
     }
 }
